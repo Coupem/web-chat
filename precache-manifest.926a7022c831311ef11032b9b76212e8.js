fb5f944ac66a73753f445bc6ef049981d768520f
Dmitriy.Coupem <dmitriycoupem@gmail.com>
2021-12-14 16:46:04 +0200

    Updates

---
+++ precache-manifest.926a7022c831311ef11032b9b76212e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9457e437233aa9f07dbcb555f7c3258c",
+    "url": "/web-chat/index.html"
+  },
+  {
+    "revision": "2c75afc6d027e48e5167",
+    "url": "/web-chat/static/js/2.4d6e857d.chunk.js"
+  },
+  {
+    "revision": "f6ee193085bd72c12a3f150d210d9ebd",
+    "url": "/web-chat/static/js/2.4d6e857d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fcf5222360ae5cade17",
+    "url": "/web-chat/static/js/main.c8ed2802.chunk.js"
+  },
+  {
+    "revision": "8be9b229aab346534618",
+    "url": "/web-chat/static/js/runtime-main.9a08efd6.js"
+  }
+]);
